38267ee23e1212e19b8505773cccd3890c93a042
torhilf <torhilf@gmail.com>
2024-10-29 00:15:54 +0300

    callback
    callback

--- lessons.js
+++ lessons.js
@@ -34,6 +34,7 @@ const myData = `my name is ${myName} ${mySurname} and i am ${myJob}`
 console.log(myData)
 */ 
 
+/* lesson3
 const myString = '';
 const myNumber = 5;
 const myBoolean = true;
@@ -49,3 +50,64 @@ console.log(typeof myNull);
 console.log(typeof myUndefined);
 console.log(typeof myObject);
 console.log(typeof myArray);
+*/
+
+/* lesson4
+function printMyName() {
+    const myName = 'sergei'
+    console.log(myName)
+};
+
+printMyName()
+*/
+
+/*name function
+const hello = (name) => {
+    return `hello ${name}`
+}
+
+console.log(hello('sergei'))
+
+console.log(hello('anton'))
+*/
+
+/* явное возвращение значений
+const helloWorld = () => {
+    return 'hi, world'
+}
+
+console.log(helloWorld())
+
+// неявное возврашение значений. Синтаксис используется, когда функция состоит из 1 выражения
+
+const helWorld2 = () => 'wow'
+
+console.log(helWorld2())
+*/
+
+/* Как добавлять описание для функций, JSdoc
+/**
+ * Возвращает разницу двух чисел
+ * @param {number} first 
+ * @param {number} second 
+ * @returns {number} Разница чисел
+ */
+
+/*
+const myMinus = (first, second) => {
+    return first - second
+}
+
+console.log(myMinus(10, 2))
+*/
+
+/* callback функция с таймером
+
+const myCallBack = () => {
+    console.log('hello world')
+}
+
+setTimeout(myCallBack, 3000); // вызвал для 
+*/
+
+setTimeout(() => console.log('в одну строку'), 5000)
