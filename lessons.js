588e1c0f415b5f1c6bd3a76d3ca794e85dcd524d
torhilf <torhilf@gmail.com>
2024-10-28 23:20:14 +0300

    lessons
    test lessons

---
+++ lessons.js
@@ -0,0 +1,51 @@
+/* 1st lesson
+let isStudent
+
+isStudent = true
+
+console.log(isStudent)
+
+isStudent = false
+
+console.log(isStudent)
+
+let a = 10;
+
+let b = 20;
+
+let result = a * b;
+
+console.log(result)
+
+const myNumber1 = 8;
+const myNumber2 = 15;
+
+const mult = myNumber1 * myNumber2;
+console.log(mult)
+
+1st lesson */ 
+
+/* 2st lesson
+const myName = 'sergei';
+const mySurname = 'buneev';
+const myJob = 'developer';
+
+const myData = `my name is ${myName} ${mySurname} and i am ${myJob}`
+console.log(myData)
+*/ 
+
+const myString = '';
+const myNumber = 5;
+const myBoolean = true;
+const myNull = null;
+const myUndefined = undefined;
+const myObject = {};
+const myArray = [];
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(typeof myNull);
+console.log(typeof myUndefined);
+console.log(typeof myObject);
+console.log(typeof myArray);
